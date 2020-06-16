1b625a0e5a4a170ef296c96db1cbbda02528c94e
vstoms <vstoms@gmail.com>
2020-06-16 09:33:20 +0200

    Update exo-address-book-policy.ps1

--- exo-address-book-policy.ps1
+++ exo-address-book-policy.ps1
@@ -1,32 +1,32 @@
 # Configuration for students:
 
-New-AddressList -Name "AL-EDU-Users-DGs" -RecipientFilter {((RecipientTypeDetails -eq 'UserMailbox') -or (RecipientTypeDetails -eq "MailUniversalDistributionGroup") -or (RecipientTypeDetails -eq "DynamicDistributionGroup")) -and (CustomAttribute5 -eq "EDU")}
+New-AddressList -Name "AL-EDU-Users-DGs" -RecipientFilter {((RecipientTypeDetails -eq 'UserMailbox') -or (RecipientTypeDetails -eq "MailUniversalDistributionGroup") -or (RecipientTypeDetails -eq "DynamicDistributionGroup")) -and (CustomAttribute3 -eq "BLANK")}
 
-New-AddressList -Name "AL-EDU-Rooms" -RecipientFilter {((Alias -ne $null) -and ((RecipientDisplayType -eq 'ConferenceRoomMailbox') -or (RecipientDisplayType -eq 'SyncedConferenceRoomMailbox'))) -and (CustomAttribute5 -eq "EDU")}
+New-AddressList -Name "AL-EDU-Rooms" -RecipientFilter {((Alias -ne $null) -and ((RecipientDisplayType -eq 'ConferenceRoomMailbox') -or (RecipientDisplayType -eq 'SyncedConferenceRoomMailbox'))) -and (CustomAttribute5 -eq "BLANK")}
 
-New-GlobalAddressList -Name "GAL-EDU" -RecipientFilter {(CustomAttribute5 -eq "EDU")}
+New-GlobalAddressList -Name "GAL-EDU" -RecipientFilter {(CustomAttribute3 -eq "BLANK")}
 
-New-OfflineAddressBook -Name "OAB-EDU" -AddressLists "AL-EDU-Users-DGs"
+New-OfflineAddressBook -Name "OAB-EDU" -AddressLists "GAL-EDU"
 
 New-AddressBookPolicy -Name "ABP-EDU" -AddressLists "AL-EDU-Users-DGs","AL-EDU-Rooms" -OfflineAddressBook "\OAB-EDU" -GlobalAddressList "\GAL-EDU" -RoomList "\AL-EDU-Rooms"
 
-Get-Mailbox | Where {$_.CustomAttribute5 -eq "EDU"} | Set-Mailbox -AddressBookPolicy "ABP-EDU"
+Get-Mailbox | Where {$_.CustomAttribute3 -eq "Student"} | Set-Mailbox -AddressBookPolicy "ABP-EDU"
 
 
 
 # Configuration for administrator personnel, managers and teachers:
 
-New-AddressList -Name "AL-ADM-Users-DGs" -RecipientFilter {((RecipientTypeDetails -eq 'UserMailbox') -or (RecipientType -eq "MailUniversalDistributionGroup") -or (RecipientType -eq "DynamicDistributionGroup")) -and (CustomAttribute5 -eq "ADM")}
+New-AddressList -Name "AL-ADM-Users-DGs" -RecipientFilter {((RecipientTypeDetails -eq 'UserMailbox') -or (RecipientType -eq "MailUniversalDistributionGroup") -or (RecipientType -eq "DynamicDistributionGroup")) -and (CustomAttribute1 -eq "Employee")}
 
-New-AddressList -Name "AL-ADM-Rooms" -RecipientFilter  {((Alias -ne $null) -and ((RecipientDisplayType -eq 'ConferenceRoomMailbox') -or (RecipientDisplayType -eq 'SyncedConferenceRoomMailbox'))) -and (CustomAttribute5 -eq "ADM")}
+New-AddressList -Name "AL-ADM-Rooms" -RecipientFilter  {((Alias -ne $null) -and ((RecipientDisplayType -eq 'ConferenceRoomMailbox') -or (RecipientDisplayType -eq 'SyncedConferenceRoomMailbox'))) -and (CustomAttribute1 -eq "Employee")}
 
-New-GlobalAddressList -Name "GAL-ADM" -RecipientFilter {(CustomAttribute5 -eq "ADM")}
+New-GlobalAddressList -Name "GAL-ADM" -RecipientFilter {(CustomAttribute1 -eq "Employee")}
 
 New-OfflineAddressBook -Name "OAB-ADM" -AddressLists "GAL-ADM"
 
 New-AddressBookPolicy -Name "ABP-ADM" -AddressLists "AL-ADM-Users-DGs","AL-ADM-Rooms" -OfflineAddressBook "\OAB-ADM" -GlobalAddressList "\GAL-ADM" -RoomList "\AL-ADM-Rooms"
 
-Get-Mailbox | Where {$_.CustomAttribute5 -eq "ADM"}  | Set-Mailbox -AddressBookPolicy "ABP-ADM"
+Get-Mailbox | Where {$_.CustomAttribute1 -eq "Employee"}  | Set-Mailbox -AddressBookPolicy "ABP-ADM"
 
 
 
